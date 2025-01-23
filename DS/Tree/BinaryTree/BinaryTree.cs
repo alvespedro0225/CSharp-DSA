360af306726b7c2c3a76fe0109b5770cbadc1e5e
Pedro <alvespedro0225@gmail.com>
2025-01-22 22:38:05 -0300

    formatting

---
+++ DS/Tree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,78 @@
+namespace BinaryTree;
+
+public struct BinaryTree<T> where T : IComparable<T>
+{
+    public class Node<TNode> where TNode : IComparable<TNode>
+    {
+        internal Node<TNode>? Left;
+        internal Node<TNode>? Right;
+        public TNode? Value;
+    }
+    private Node<T>? _root;
+
+    public int Size => _root == null ? 0 : _Size(_root);
+    public int Height => _root == null ? 0 : _Height(_root);
+
+    public void Add(T value)
+    {
+        Node<T> newNode = new()
+        {
+            Value = value,
+        };
+        if (_root == null)
+        {
+            _root = newNode;
+            return;
+        }
+        var current = _root;
+        Node<T>? previous = null;
+        var bigger = false;
+        while (current != null)
+        {
+            previous = current;
+            switch (newNode.Value.CompareTo(current.Value))
+            {
+                case > 0:
+                    bigger = true;
+                    current = current.Right;
+                    break;
+                case < 0:
+                    bigger = false;
+                    current = current.Left;
+                    break;
+                case 0:
+                    Console.WriteLine("Node already exists");
+                    return;
+            }
+        }
+        if (bigger) previous!.Right = newNode;
+        else previous!.Left = newNode;
+    }
+    public void Print()
+    {
+        if (_root == null)
+        {
+            Console.WriteLine("Tree is empty");
+            return;
+        }
+        Print(_root);
+    }
+    private static void Print(Node<T>? node, string indent = "")
+    {
+        if (node is null) return;
+        Console.WriteLine($"{indent}{node.Value}");
+        Print(node.Left, indent + " ");
+        Print(node.Right, indent + " ");
+    }
+
+    private static int _Height(Node<T>? node)
+    {
+        if (node == null) return 0;
+        return 1 + Math.Max(_Height(node.Left), _Height(node.Right));
+    }
+    private static int _Size(Node<T>? node)
+    {
+        if (node == null) return 0;
+        return 1 + _Size(node.Left) + _Size(node.Right);
+    }
+}
